12e89642dcbce1c9e47a2ccc011b67a8f692f780
Shah Md. Imran Hossain <akashganga.programming.534363@gmail.com>
2023-05-21 23:04:34 +0600

    Constants

---
+++ DrawingApp/Constants.swift
@@ -0,0 +1,21 @@
+//
+//  Constants.swift
+//  DrawingApp
+//
+//  Created by Shah Md Imran Hossain on 21/5/23.
+//
+
+import Foundation
+
+struct Constants {
+    struct Icon {
+        static let plusCircle = "plus.circle"
+        static let line3HorizontalCircleFill = "line.3.horizontal.circle.fill"
+        static let circle = "circle"
+        static let circleInsetFilled = "circle.inset.filled"
+        static let exclaimationMarkCircle = "exclamationmark.circle"
+        static let recordCircleFill = "record.circle.fill"
+        static let trayCircleFill = "tray.circle.fill"
+        static let circleFill = "circle.fill"
+    }
+}
